7e59f72179d379bd51ef568babf7883d7c97c65a
Tony Locke <tlocke@tlocke.org.uk>
2015-08-03 19:51:31 +0100

    Have input file as an argument

--- mooc.py
+++ mooc.py
@@ -1,7 +1,13 @@
 import csv
 
+import argparse
+
+parser = argparse.ArgumentParser(description='Restructure MOOC CSV.')
+parser.add_argument('input_csv', type=open)
+args = parser.parse_args()
+
 data_in = {}
-with open('input.csv', 'r') as csvin:
+with args.input_csv as csvin:
     reader = csv.reader(csvin)
     next(reader)  # skip titles
     for cid, author_id, parent_cid in reader:
