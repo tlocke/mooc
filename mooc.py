991ed46bcc0886e310c43b33ada4aad0d95991a0
Tony Locke <tlocke@tlocke.org.uk>
2015-08-05 19:27:49 +0100

    Add text of comments on the end of the output

--- mooc.py
+++ mooc.py
@@ -10,16 +10,21 @@ data_in = {}
 with args.input_csv as csvin:
     reader = csv.reader(csvin)
     next(reader)  # skip titles
-    for cid, author_id, parent_cid in reader:
-        data_in[cid] = (author_id, parent_cid.strip())
+    for cid, author_id, parent_cid, step, txt, *x in reader:
+        data_in[cid] = (author_id, parent_cid.strip(), txt)
 
-data_out = collections.defaultdict(int)
-for source_author_id, parent_cid in data_in.values():
+data_out = {}
+for source_author_id, parent_cid, txt in data_in.values():
     if len(parent_cid) > 0:
-        data_out[(source_author_id, data_in[parent_cid][0])] += 1
+        k = (source_author_id, data_in[parent_cid][0])
+        try:
+            v = data_out[k]
+            v[0] += 1
+            v[1] += txt
+        except KeyError:
+            data_out[k] = [1, txt]
 
 with open('output.csv', 'w') as csvout:
     writer = csv.writer(csvout)
-    writer.writerow(['source', 'target', 'weight'])
-    for k, v in data_out.items():
-        writer.writerow(list(k) + [v])
+    writer.writerow(['source', 'target', 'weight', 'comments'])
+    writer.writerows(list(k) + v for k, v in data_out.items())
